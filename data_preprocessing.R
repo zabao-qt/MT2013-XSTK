9bf9eeab9c3e2c57708c7b1fce85982d938c4a20
zabao-qt <tagiabao.qt@gmail.com>
2025-07-25 01:07:00 +0700

    fix ROPs data type

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -16,6 +16,11 @@ gpu_clean$Max_Power <- as.numeric(str_remove(gpu_clean$Max_Power, " Watts"))
 gpu_clean$Memory_Bus <- as.numeric(str_remove(gpu_clean$Memory_Bus, " Bit"))
 gpu_clean$Process <- as.numeric(str_remove(gpu_clean$Process, "nm"))
 gpu_clean$Memory <- as.numeric(str_remove(gpu_clean$Memory, " MB"))
+gpu_clean$ROPs <- ifelse(
+  str_detect(gpu_clean$ROPs, "\\(x2\\)"),
+  as.numeric(str_extract(gpu_clean$ROPs, "\\d+")) * 2,
+  as.numeric(str_extract(gpu_clean$ROPs, "\\d+"))
+)
 
 summary(gpu_clean)
 
@@ -29,12 +34,13 @@ gpu_clean <- gpu_clean %>%
          !is.na(Memory_Bandwidth), 
          !is.na(Memory_Bus))
 
-cols_median_fill <- c("Core_Speed", "Process", "Max_Power", "TMUs", "Memory")
+gpu_clean$Core_Speed[is.na(gpu_clean$Core_Speed)] <- median(gpu_clean$Core_Speed, na.rm = TRUE)
+gpu_clean$Process[is.na(gpu_clean$Process)] <- median(gpu_clean$Process, na.rm = TRUE)
+gpu_clean$Max_Power[is.na(gpu_clean$Max_Power)] <- median(gpu_clean$Max_Power, na.rm = TRUE)
+gpu_clean$TMUs[is.na(gpu_clean$TMUs)] <- median(gpu_clean$TMUs, na.rm = TRUE)
+gpu_clean$Memory[is.na(gpu_clean$Memory)] <- median(gpu_clean$Memory, na.rm = TRUE)
+gpu_clean$ROPs[is.na(gpu_clean$ROPs)] <- median(gpu_clean$ROPs, na.rm = TRUE)
 
-for (col in cols_median_fill) {
-  median_value <- median(gpu_clean[[col]], na.rm = TRUE)
-  gpu_clean[[col]][is.na(gpu_clean[[col]])] <- median_value
-}
 
 apply(is.na(gpu_clean),2,sum)
 apply(is.na(gpu_clean),2,mean)
