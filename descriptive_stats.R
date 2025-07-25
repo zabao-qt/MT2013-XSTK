016feeb254af0b79b28d2744b428c558d67e52f8
Nguyen Dinh Huy <huy.nguyen060906@hcmut.edu.vn>
2025-07-25 23:49:52 +0700

    Bieu do

--- descriptive_stats.R
+++ descriptive_stats.R
@@ -1,25 +1,37 @@
+source("data_preprocessing.R")
 library(ggplot2)
 
 # PHAN 4 Thong ke ta
-
 # Du lieu dinh tinh
+data1 <- gpu_clean[, c("Manufacturer"), drop = FALSE]
 barplot(
-  table(gpu_clean$Manufacturer),
+  table(data1$Manufacturer),
   xlab = "Manufacturer",
   ylab = "Frequency",
   main = "Bar Plot of GPU Manufacturer",
-  col = "steelblue",
+  ylim = c(0, 2000),
+  col = "gray",
   border = "black"
 )
 
 # Du lieu dinh luong
-boxplot(Max_Power ~ Manufacturer,
-        data = gpu_clean,
-        xlab = "Manufacturer",
-        ylab = "Max_Power",
-        main = "Manufacturer vs TMUs",
-        pch = 20,
-        cex = 1.5,
-        col = "lightgreen",
-        border = "black")
+data2 <- gpu_clean[, c("Core_Speed", "Memory", "Memory_Bandwidth",
+                        "Memory_Speed", "Memory_Bus", "Process",
+                        "Max_Power", "TMUs", "ROPs"), drop = FALSE]
+hist(data2$Core_Speed,
+     main = "Histogram of Core_Speed",
+     xlab = "Core_Speed",
+     ylab = "Frequency",
+     xlim = c(0, 2000),
+     ylim = c(0, 1500),
+     border = "black")
 
+plot(Max_Power ~ Process, data = data2,
+     xlab = "Process (nm)",
+     ylab = "Max_Power (Watts)",
+     main = "Max_Power vs Process",
+     pch = 16,
+     cex = 2,
+     xlim = c(0, 160),
+     ylim = c(0, 900),
+     col = "gray30")
